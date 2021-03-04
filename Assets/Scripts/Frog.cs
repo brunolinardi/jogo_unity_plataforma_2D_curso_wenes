5d537e29c54fb3976191d17919edfdebc7ff5e71
brunolinardi <brunolinardi@gmail.com>
2021-03-04 09:22:25 -0300

    Version_0.1
    Versão 0.1 do jogo do curso

---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+    public float speed;
+    public Transform rightCol;
+    public Transform leftCol;
+    public Transform headPoint;
+    private bool colliding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position);
+
+        if(colliding)
+        {
+            
+        }
+    }
+}
